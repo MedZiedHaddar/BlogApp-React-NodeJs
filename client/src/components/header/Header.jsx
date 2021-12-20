7837a3f9028f1c4056d8af52d16eca80a6fc57c2
MedZiedHaddar <medzied.haddar@gmail.com>
2021-12-20 16:08:16 +0100

    initial commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import './header.css'
+
+export default function header() {
+    return (
+        <div className="header">
+            <div className="headerTitles">
+                <span className="headerTitleSm">Technology</span>
+                <span className="headerTitleLg">Geeks</span>
+            </div>
+
+            <img
+            className="headerImg"
+            src="https://thetechtheories.com/wp-content/uploads/2020/01/What-is-the-future-of-Technology.jpg"
+            alt=""
+            />
+            <br/><br/><br/><br/>
+        </div>
+    )
+}
